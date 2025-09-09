7cf82ba59a18244727d6e76079aee8bab06ca4d9
Taufique <Taufiquemeraj.ansari@cognizant.com>
2025-09-09 21:42:33 +0530

    Initial codebase skeleton

---
+++ backend/DigitalBookstoreManagementSystem/src/main/java/com/example/DigitalBookstoreManagementSystem/DigitalBookstoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.DigitalBookstoreManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBookstoreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBookstoreManagementSystemApplication.class, args);
+	}
+
+}
